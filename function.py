22b0179218d4f672b1f16ee088b3f2194ef0dfd6
Yashwanth Kumar <36370530+yashwanth2714@users.noreply.github.com>
2025-08-18 10:21:29 +0530

    default params

--- function.py
+++ function.py
@@ -1,7 +1,11 @@
-def hello(to):
+# Default parameters
+# If no argument is passed, "World" will be used as the default value
+def hello(to="World"):
     # to is a parameter of the hello function
     print("Hello,",to)
 
+
+hello() # prints "Hello, World"
 
 name = input("What's your name? " )
 
