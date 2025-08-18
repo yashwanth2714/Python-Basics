f56139abd1ad7c33337e3d520a88a1440f5ac6b7
Yashwanth Kumar <36370530+yashwanth2714@users.noreply.github.com>
2025-08-18 10:01:41 +0530

    float and round

--- calculator.py
+++ calculator.py
@@ -1,4 +1,9 @@
-x = int(input("What's x? "))
-y = int(input("What's y? "))
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
 
-print(x + y)
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+z = round(x + y)
+
+print(z)
