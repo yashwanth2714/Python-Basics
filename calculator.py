0144601ee71543fd962f9ef14da6b18ff6a6fb5e
Yashwanth Kumar <36370530+yashwanth2714@users.noreply.github.com>
2025-08-18 10:08:48 +0530

    formatting techniques

--- calculator.py
+++ calculator.py
@@ -6,4 +6,12 @@ y = float(input("What's y? "))
 
 z = round(x + y)
 
+# Format the output with commas as thousands separators
+# The :, format specifier adds commas as thousands separators
 print(f"{z:,}")
+
+a = x / y
+
+# Format the output to 4 decimal places
+# The :.4f format specifier formats the number to 4 decimal places
+print (f"{a:.4f}")
