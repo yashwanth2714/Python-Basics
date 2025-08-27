335bbf7198d788bc0c7659318b2cbd10da2d2417
Yashwanth Kumar <36370530+yashwanth2714@users.noreply.github.com>
2025-08-27 20:25:06 +0530

    Update conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -34,11 +34,21 @@ score = int(input("Score: "))
 #     print("Grade F")
 
 
-if 90 <= score <= 100:
+# if 90 <= score <= 100:
+#     print("Grade A")
+# elif 80 <= score < 90:
+#     print("Grade B")
+# elif 70 <= score < 80:
+#     print("Grade C")
+# else:
+#     print("Grade F")
+
+
+if score >= 90:
     print("Grade A")
-elif 80 <= score < 90:
+elif score >= 80:
     print("Grade B")
-elif 70 <= score < 80:
+elif score >= 70:
     print("Grade C")
 else:
     print("Grade F")
