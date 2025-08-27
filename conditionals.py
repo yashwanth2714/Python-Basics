f0ef98a718edf87a1c76b5e7ec29298819900c16
Yashwanth Kumar <36370530+yashwanth2714@users.noreply.github.com>
2025-08-27 20:14:57 +0530

    Operators

--- conditionals.py
+++ conditionals.py
@@ -1,10 +1,33 @@
-x = int(input("What's x? "))
-y = int(input("What's y? "))
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
 
 
-if x < y:
-    print("x is less than y")
-elif x > y:
-    print("x is greater than y")
+# if x < y:
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# else:
+#     print("x is equal to y")
+
+
+# if x < y or x > y:
+#     print ("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+
+# if x != y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+
+score = int(input("Score: "))
+if score >= 90 and score <=100:
+    print("Grade A")
+elif score >= 80 and score < 90:
+    print("Grade B")
+elif score >= 70 and score < 80:
+    print("Grade C")
 else:
-    print("x is equal to y")
+    print("Grade F")
