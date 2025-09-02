ff4b323abee36c85fcbf36c42dacac9d3b679690
Yashwanth Kumar <36370530+yashwanth2714@users.noreply.github.com>
2025-09-02 19:56:43 +0530

    validating inputs

--- loops.py
+++ loops.py
@@ -1,32 +1,43 @@
-i = 0
+# i = 0
 
-while i < 3:
-    print("hola", i, sep="-")
-    i = i + 1
+# while i < 3:
+#     print("hola", i, sep="-")
+#     i = i + 1
     
 
-for i in [0,1,2]:
-    print("hello", i, sep="*")
+# for i in [0,1,2]:
+#     print("hello", i, sep="*")
     
     
-# range function generates a sequence of numbers
-# range(3) generates numbers from 0 to 2
-# range(start, stop) generates numbers from start to stop-1. For example, range(1,4) generates 1,2,3
-# range(start, stop, step) generates numbers from start to stop-1 with a step. For example, range(1,10,2) generates 1,3,5,7,9
-for i in range(3):
-    print("hi", i, sep="_")
+# # range function generates a sequence of numbers
+# # range(3) generates numbers from 0 to 2
+# # range(start, stop) generates numbers from start to stop-1. For example, range(1,4) generates 1,2,3
+# # range(start, stop, step) generates numbers from start to stop-1 with a step. For example, range(1,10,2) generates 1,3,5,7,9
+# for i in range(3):
+#     print("hi", i, sep="_")
     
     
-# _ is a throwaway variable
-# It is used when you don't need the variable. For example, when you just want to repeat something a certain number of times
-for _ in range(3):
-    print("hey")
+# # _ is a throwaway variable
+# # It is used when you don't need the variable. For example, when you just want to repeat something a certain number of times
+# for _ in range(3):
+#     print("hey")
 
 
-# You can multiply strings in Python
-# prints "meowmeowmeow"
-print("meow" * 3)
+# # You can multiply strings in Python
+# # prints "meowmeowmeow"
+# print("meow" * 3)
 
 
-# end="" prevents the extra newline that print adds by default
-print("meow\n" * 3, end="")
+# # end="" prevents the extra newline that print adds by default
+# print("meow\n" * 3, end="")
+
+
+# Ask user for a positive integer. Keep asking until they provide a positive integer.
+# This is a common pattern for input validation.
+while True:
+    n= int(input("What's n? "))
+    if n > 0:
+        break
+    
+for _ in range(n):
+    print("meow")
