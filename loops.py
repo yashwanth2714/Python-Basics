ebb504ae7b0def37e696610db641220f0f262a98
Yashwanth Kumar <36370530+yashwanth2714@users.noreply.github.com>
2025-09-02 19:41:30 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,23 @@
+i = 0
+
+while i < 3:
+    print("hola", i, sep="-")
+    i = i + 1
+    
+
+for i in [0,1,2]:
+    print("hello", i, sep="*")
+    
+    
+# range function generates a sequence of numbers
+# range(3) generates numbers from 0 to 2
+# range(start, stop) generates numbers from start to stop-1. For example, range(1,4) generates 1,2,3
+# range(start, stop, step) generates numbers from start to stop-1 with a step. For example, range(1,10,2) generates 1,3,5,7,9
+for i in range(3):
+    print("hi", i, sep="_")
+    
+    
+# _ is a throwaway variable
+# It is used when you don't need the variable. For example, when you just want to repeat something a certain number of times
+for _ in range(3):
+    print("hey")
