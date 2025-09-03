05089ae7ac04ef228a4b2e33e44191e09bab772b
Yashwanth Kumar <36370530+yashwanth2714@users.noreply.github.com>
2025-09-03 11:04:25 +0530

    else and finally blocks

--- exceptions.py
+++ exceptions.py
@@ -1,8 +1,20 @@
+# try:
+#     x = int(input("What's x? "))
+# except ValueError:
+#     print("x is not an integer")
+    
+# # when you enter a non-integer value, the program will not crash. Instead, it will print "x is not an integer"
+# # But, the variable x will not be defined if the user enters a non-integer value because the assignment to x is inside the try block. And, if an exception occurs, the assignment is skipped.
+# print(f"x is {x}")
+
+
 try:
     x = int(input("What's x? "))
 except ValueError:
     print("x is not an integer")
-    
-# when you enter a non-integer value, the program will not crash. Instead, it will print "x is not an integer"
-# But, the variable x will not be defined if the user enters a non-integer value because the assignment to x is inside the try block. And, if an exception occurs, the assignment is skipped.
-print(f"x is {x}")
+else:
+    # else block is executed if no exception occurs in the try block. If an exception occurs, the else block is skipped.
+    print(f"x is {x}")
+finally:
+    # finally block is always executed, regardless of whether an exception occurs or not. It is typically used for cleanup actions.
+    print("This is the finally block. It is always executed.")
