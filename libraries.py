e0b28dce9dcaa423de29445432dfaeb1b7a604c1
Yashwanth Kumar <36370530+yashwanth2714@users.noreply.github.com>
2025-09-05 17:29:10 +0530

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -1,4 +1,15 @@
-from random import choice
+from random import choice, randint, shuffle
 
+# choice is 
 coin = choice(["heads", "tails"])
 print(coin)
+
+
+# randint returns a random integer between the two numbers (inclusive)
+print(randint(1, 6))
+
+
+cards = ["Jack", "Queen", "King", "Ace"]
+# shuffle is an in-place shuffle, it modifies the original list
+shuffle(cards)
+print(cards)
